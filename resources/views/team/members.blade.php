c20914216552e9d14ed673e752590ab88f6b335e
三井友輔 <mitsui@mitsuitomotasukunoMacBook-Pro.local>
2022-03-09 16:11:21 +0900

    team.show

--- resources/views/team/members.blade.php
+++ resources/views/team/members.blade.php
@@ -25,12 +25,26 @@
     <div class = "offset-sm-2 mt-3">
         @foreach($members as $member)
         <div class = "row">
+            @if(($member->post())==2)
             <h3 class='offset-sm-1 col-sm-6'>{!!link_to_route('users.show',$member->firstName.' '.$member->lastName,[$member->id],[])!!}</h3>
             <div class = 'mt-2 col-sm-4'>
                 @if(($team->leader)==Auth::id()|($team->project()->manager())==Auth::user())
                 {{link_to_route('teams.remove','チームから外す',['team'=>$team->id,'user'=>$member->id],['class'=>'btn btn-danger btn-bock btn-sm'])}}
                 @endif
             </div>
+            @endif
+        </div>
+        @endforeach
+        @foreach($members as $member)
+        <div class = "row">
+            @if(($member->post())==1)
+            <h3 class='offset-sm-1 col-sm-6'>{!!link_to_route('users.show',$member->firstName.' '.$member->lastName,[$member->id],[])!!}</h3>
+            <div class = 'mt-2 col-sm-4'>
+                @if(($team->leader)==Auth::id()|($team->project()->manager())==Auth::user())
+                {{link_to_route('teams.remove','チームから外す',['team'=>$team->id,'user'=>$member->id],['class'=>'btn btn-danger btn-bock btn-sm'])}}
+                @endif
+            </div>
+            @endif
         </div>
         @endforeach
     </div>
